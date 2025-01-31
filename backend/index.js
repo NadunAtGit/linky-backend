cd8c413a1ef54c5a8a0d55e3773c63d6772cb9bc
NadunAtGit <nadunsoor404@gmail.com>
2025-02-01 02:48:19 +0530

    env var

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology: true }).then(()=>{
     console.log("Database connected successfully");
 
         app.listen(PORT, () => {
