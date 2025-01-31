9c8ef42c499f9e38aa0ec783e14e4d8d88b6262d
NadunAtGit <nadunsoor404@gmail.com>
2025-02-01 04:01:47 +0530

    bcrypt

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose=require("mongoose");
 
 const app=express();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const User=require("./models/UserModel");
